f68b4e9a8be6ba0f72c618135b00979208dbd884
florescu.adrian <florescu.adrian@gmail.com>
2014-03-14 15:18:05 +0000

    New configuration!

--- my_activity_log/src/parser/RunParser.java
+++ my_activity_log/src/parser/RunParser.java
@@ -1,84 +1,55 @@
 package parser;
-
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
-public class RunParser
-{
-	public static void main(String[] args) throws IOException, ParseException {
-		Config config = new Config();
-
-		int idle_count = 0;
-		int work_count = 0;
-		int procratinating_count = 0;
-		int total_idle_count = 0;
-		int total_work_count = 0;
-		int total_procratinating_count = 0;
-		String file = config.getFile();
-		BufferedReader br = null;
-		br = new BufferedReader(new FileReader(file));
-		String line;
-		Date previousDate = null;
-		Date currentDate = null;
-		while ((line = br.readLine()) != null) {
-			String dateString = line.split("-")[0];
-			SimpleDateFormat ft = new SimpleDateFormat ("hh:mm dd MMMMM yyyy");
-
-			currentDate = ft.parse(dateString);
-			if (previousDate == null)
-				previousDate = currentDate;
-			if (currentDate.getDay() != previousDate.getDay()) {
-				System.out.printf("For date %s here are the results: \n", previousDate.toLocaleString());
-				System.out.printf("\tNumber of %s occurences = %d, that's %d h\n",config.getIdle()[0], idle_count, idle_count/60);
-				System.out.printf("\tNumber of %s occurences = %d, that's %d h\n",config.getProcrastinating()[0], procratinating_count, procratinating_count/60);
-				System.out.printf("\tNumber of %s occurences = %d, that's %d h\n",config.getWork()[0], work_count, work_count/60);
-				if (work_count/60 >= 5)
-					System.out.printf("!!! Wow !!!!!! Wow !!!!!! Wow !!!!!! Wow !!!!!! Wow !!! %f\n", work_count/60.);
-
-				if (procratinating_count/60 >= 1)
-					System.out.printf("!!! HMMMMM !!!!!! HMMMMM !!!!!! HMMMMM !!!!!! HMMMMM !!!!!! HMMMMM !!! %f\n", procratinating_count/60.);
-
-				previousDate = currentDate;
-				total_idle_count += idle_count;
-				total_work_count += work_count;
-				total_procratinating_count += procratinating_count;
-				idle_count = 0;
-				work_count = 0;
-				procratinating_count = 0;
-			}
-
-			Boolean stop = false;
-			for (String word:config.getIdle())
-			{
-				if (line.matches(".*"+word+".*")){ idle_count ++; stop = true; break; }
-			}
-			if (!stop)
-			for (String word:config.getProcrastinating())
-			{
-				if (line.matches(".*"+word+".*")){ procratinating_count ++; stop = true; break; }
-			}
-			if (!stop)
-			for (String word:config.getWork())
-			{
-				if (line.matches(".*"+word+".*")){ work_count ++; break; }
-			}
+ 
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.DocumentBuilder;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Node;
+import org.w3c.dom.Element;
+import java.io.File;
+ 
+public class RunParser {
+ 
+  public static void main(String argv[]) {
+ 
+    try {
+ 
+	File fXmlFile = new File("/Users/mkyong/staff.xml");
+	DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+	DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+	Document doc = dBuilder.parse(fXmlFile);
+ 
+	//optional, but recommended
+	//read this - http://stackoverflow.com/questions/13786607/normalization-in-dom-parsing-with-java-how-does-it-work
+	doc.getDocumentElement().normalize();
+ 
+	System.out.println("Root element :" + doc.getDocumentElement().getNodeName());
+ 
+	NodeList nList = doc.getElementsByTagName("staff");
+ 
+	System.out.println("----------------------------");
+ 
+	for (int temp = 0; temp < nList.getLength(); temp++) {
+ 
+		Node nNode = nList.item(temp);
+ 
+		System.out.println("\nCurrent Element :" + nNode.getNodeName());
+ 
+		if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+ 
+			Element eElement = (Element) nNode;
+ 
+			System.out.println("Staff id : " + eElement.getAttribute("id"));
+			System.out.println("First Name : " + eElement.getElementsByTagName("firstname").item(0).getTextContent());
+			System.out.println("Last Name : " + eElement.getElementsByTagName("lastname").item(0).getTextContent());
+			System.out.println("Nick Name : " + eElement.getElementsByTagName("nickname").item(0).getTextContent());
+			System.out.println("Salary : " + eElement.getElementsByTagName("salary").item(0).getTextContent());
+ 
 		}
-
-		System.out.printf("For date %s here are the results: \n", currentDate.toLocaleString());
-		System.out.printf("\tNumber of %s occurences = %d, that's %d h\n",config.getIdle()[0], idle_count, idle_count/60);
-		System.out.printf("\tNumber of %s occurences = %d, that's %d h\n",config.getProcrastinating()[0], procratinating_count, procratinating_count/60);
-		System.out.printf("\tNumber of %s occurences = %d, that's %d h\n",config.getWork()[0], work_count, work_count/60);
-
-
-		System.out.printf("Total work is %.2f hours or %.2f days;\n", total_work_count / 60., total_work_count / 1440.);
-		System.out.printf("Total idle is %.2f hours or %.2f days;\n", total_idle_count / 60., total_idle_count / 1440.);
-		System.out.printf("Total procrastinating is %.2f hours or %.2f days;\n", total_procratinating_count / 60., total_procratinating_count / 1440.);
-
-		br.close();
 	}
+    } catch (Exception e) {
+	e.printStackTrace();
+    }
+  }
+ 
 }
